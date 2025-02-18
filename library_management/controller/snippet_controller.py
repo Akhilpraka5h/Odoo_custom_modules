a899fcf09faf0c3740744a8ab41bcfa04e24c34a
AKHIL <134758922+Akhilpraka5h@users.noreply.github.com>
2025-02-18 17:45:56 +0530

    responsive book carousel snippet

--- library_management/controller/snippet_controller.py
+++ library_management/controller/snippet_controller.py
@@ -1,5 +1,4 @@
 from odoo.http import request, Controller, route
-from odoo.tools.safe_eval import time
 
 
 class BookSnippet(Controller):
@@ -9,16 +8,35 @@ class BookSnippet(Controller):
            website=True)
     def all_books(self):
         """Function to get all book from library"""
-        current_website = request.env['website'].sudo().get_current_website().id
+        current_website = request.env['website'].sudo().get_current_website()
+        current_company = request.env.company
+        company_currency = current_company.currency_id
+        currency_symbol = company_currency.symbol
+        pricelist_currency = current_website.pricelist_id.currency_id
+        pricelist_cur_symbol = pricelist_currency.symbol
         books = request.env['library.book'].sudo().search_read([],
                                                                ['book_title',
                                                                 'book_cover_image',
                                                                 'id',
                                                                 'borrow_count',
                                                                 'book_price',
-                                                                'book_status'],
+                                                                'book_status',
+                                                                'currency_id',
+                                                                'company_id'],
                                                                order='borrow_count desc')
-        unique_id = "pc-%d" % int(time.time() * 1000)
+        currency_data = request.env['res.currency'].sudo().browse(pricelist_currency.id).read(
+                                                       ['name',
+                                                        'symbol',
+                                                        'rounding',
+                                                        'decimal_places',
+                                                        'position'])
+        currency_rate = request.env['res.currency.rate'].sudo().search_read([('currency_id','=',pricelist_currency.id)],[
+            'id','company_rate'],limit=1,order='id desc')
+# got needed data for price calculation need to calculate and display
+        print('pricelist_id',pricelist_currency.symbol)
+        print('currency_data',currency_data)
+        print('pricelist_currency.id',pricelist_currency.id)
+        print('currency_rate',currency_rate)
         return {'all_books': books,
-                'website': current_website,
-                'unique_id': unique_id}
+                'website': current_website.id,
+                'currency': currency_symbol if company_currency.id == pricelist_currency.id else pricelist_cur_symbol }
--- library_management/static/src/js/book_dynamic_snippet.js
+++ library_management/static/src/js/book_dynamic_snippet.js
@@ -10,20 +10,51 @@ export function _chunk(array, size) {
     }
     return result;
 }
+function uniqueIdGenerator() {
+    return Date.now() + Math.random().toString(28).slice(2);
+}
+function getChunkSize() {
+    return window.innerWidth < 768 ? 1 : 4;
+}
 var LatestBooks = PublicWidget.Widget.extend({
-        selector: '.best_latest_books_snippet',
-        willStart: async function () {
-            const data = await rpc('/latest_library_books', {})
-            this.books = data
-        },
-        start: function () {
-            const refEl = this.$el.find("#latest_book_carousel")
-            const chunkData = _chunk(this.books.all_books, 4)
-            refEl.html(renderToElement('library_management.latest_book_snippet_carousel', {
-            chunkData
-            }))
-            console.log("chunkData",chunkData)
+    selector: '.best_latest_books_snippet',
+    willStart: async function () {
+        const data = await rpc('/latest_library_books', {});
+        this.books = data;
+    },
+    start: function () {
+        const refEl = this.$el.find("#latest_book_carousel");
+        const unique_id = uniqueIdGenerator();
+        const chunkSize = getChunkSize();
+        const all_books = this.books.all_books;
+        const currency = this.books['currency'];
+        const chunkData = _chunk(all_books, chunkSize);
+        if (all_books.length !== 0) {
+            chunkData[0].is_active = true;
         }
-    });
+        refEl.html(renderToElement('library_management.latest_book_snippet_carousel', { chunkData,currency }));
+        const slide = this.$el.find(`#latest_book_carousel`);
+        slide.find('.carousel-control-prev').attr('href', `#carousel${unique_id}`);
+        slide.find('.carousel-control-next').attr('href', `#carousel${unique_id}`);
+        slide.find('#library_latest_books_carousel').attr('id', `carousel${unique_id}`);
+
+        window.addEventListener("resize", () => {
+            this.Resize(refEl, all_books, unique_id);
+        });
+    },
+    Resize: function (refEl, all_books, unique_id) {
+        const chunkSize = getChunkSize();
+        const currency = this.books['currency'];
+        const chunkData = _chunk(all_books, chunkSize);
+        if (all_books.length !== 0) {
+            chunkData[0].is_active = true;
+        }
+        refEl.html(renderToElement('library_management.latest_book_snippet_carousel', { chunkData, currency }));
+        const slide = this.$el.find(`#latest_book_carousel`);
+        slide.find('.carousel-control-prev').attr('href', `#carousel${unique_id}`);
+        slide.find('.carousel-control-next').attr('href', `#carousel${unique_id}`);
+        slide.find('#library_latest_books_carousel').attr('id', `carousel${unique_id}`);
+    }
+});
 PublicWidget.registry.book_wise_snippet = LatestBooks;
-return LatestBooks;
+export default LatestBooks;
