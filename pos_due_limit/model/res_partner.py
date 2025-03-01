bb2cdbffc89b725c8df4ef918d08e7a2d343eb3b
AKHIL <134758922+Akhilpraka5h@users.noreply.github.com>
2025-03-01 16:40:20 +0530

    removed not neccessary files

--- pos_due_limit/model/res_partner.py
+++ pos_due_limit/model/res_partner.py
@@ -42,26 +42,6 @@ class ResPartner(models.Model):
         for partner in self:
             partner.credit_balance = round(
                 (partner.credit_limit - partner.credit_used), 2)
-            #
-            # if partner.credit_limit:
-            #     pos_orders = self.env['pos.order'].search([
-            #         ('partner_id', '=', partner.id),
-            #         ('state', 'in', ['paid', 'done', 'invoiced'])
-            #     ])
-            # credit_payment = 0.0
-            # paid_credit_payment = 0.0
-            # for order in pos_orders:
-            #     for payment in order.payment_ids:
-            #         if payment.payment_method_id.name == 'Customer Account':
-            #             credit_payment += payment.amount
-            # print(credit_payment)
-            # for order in pos_orders:
-            #     if order.account_move and order.account_move.payment_state == 'paid':
-            #         for payment in order.payment_ids:
-            #             if payment.payment_method_id.name == 'Customer Account':
-            #                 paid_credit_payment += payment.amount
-            # print(paid_credit_payment)
-            # print('credit amount', (credit_payment - paid_credit_payment))
 
     @api.model
     def _load_pos_data_fields(self, config_id):
