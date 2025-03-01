f23e0275d57e7ac770c83579306620b161c6434a
AKHIL <134758922+Akhilpraka5h@users.noreply.github.com>
2025-03-01 15:16:44 +0530

    update available credit limit

--- pos_due_limit/model/res_partner.py
+++ pos_due_limit/model/res_partner.py
@@ -17,26 +17,51 @@ class ResPartner(models.Model):
         self.credit_used = 0.0
         for partner in self:
             if partner.credit_limit:
-                used_credit = self.env['pos.order'].search([
+                pos_orders = self.env['pos.order'].search([
                     ('partner_id', '=', partner.id),
-                    ('state', 'in', ['paid', 'done', 'invoiced']),
-                    ('payment_ids.payment_method_id.name', '=',
-                     'Customer Account')
-                ]).mapped('amount_total')
-                paid_amount = self.env['pos.order'].search([
-                    ('partner_id', '=', partner.id),
-                    ('state', 'in', ['paid', 'done', 'invoiced']),
-                    ('payment_ids.payment_method_id.name', '=',
-                     'Customer Account'),
-                    ('account_move.payment_state', '=', 'paid')
-                ]).mapped('amount_total')
-                partner.credit_used = sum(used_credit) - sum(paid_amount)
+                    ('state', 'in', ['paid', 'done', 'invoiced'])
+                ])
+                credit_payment = 0.0
+                paid_credit_payment = 0.0
+                for order in pos_orders:
+                    for payment in order.payment_ids:
+                        if payment.payment_method_id.name == 'Customer Account':
+                            credit_payment += payment.amount
+                print(credit_payment)
+                for order in pos_orders:
+                    if order.account_move and order.account_move.payment_state == 'paid':
+                        for payment in order.payment_ids:
+                            if payment.payment_method_id.name == 'Customer Account':
+                                paid_credit_payment += payment.amount
+                print(paid_credit_payment)
+                print('credit amount', (credit_payment - paid_credit_payment))
+                partner.credit_used = credit_payment - paid_credit_payment
 
     @api.depends('credit_limit', 'credit_used')
     def _compute_credit_available(self):
         for partner in self:
             partner.credit_balance = round(
                 (partner.credit_limit - partner.credit_used), 2)
+            #
+            # if partner.credit_limit:
+            #     pos_orders = self.env['pos.order'].search([
+            #         ('partner_id', '=', partner.id),
+            #         ('state', 'in', ['paid', 'done', 'invoiced'])
+            #     ])
+            # credit_payment = 0.0
+            # paid_credit_payment = 0.0
+            # for order in pos_orders:
+            #     for payment in order.payment_ids:
+            #         if payment.payment_method_id.name == 'Customer Account':
+            #             credit_payment += payment.amount
+            # print(credit_payment)
+            # for order in pos_orders:
+            #     if order.account_move and order.account_move.payment_state == 'paid':
+            #         for payment in order.payment_ids:
+            #             if payment.payment_method_id.name == 'Customer Account':
+            #                 paid_credit_payment += payment.amount
+            # print(paid_credit_payment)
+            # print('credit amount', (credit_payment - paid_credit_payment))
 
     @api.model
     def _load_pos_data_fields(self, config_id):
