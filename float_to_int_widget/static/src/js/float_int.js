aaa392177ff06384706f940ff388380e3ff615e1
AKHIL <134758922+Akhilpraka5h@users.noreply.github.com>
2025-03-14 17:31:40 +0530

    options

--- float_to_int_widget/static/src/js/float_int.js
+++ float_to_int_widget/static/src/js/float_int.js
@@ -3,14 +3,20 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { useInputField } from "@web/views/fields/input_field_hook";
 import { Component } from "@odoo/owl";
-import { useRef, onWillRender, onMounted } from "@odoo/owl"
+import { useRef, onWillRender, onMounted } from "@odoo/owl";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
 
 export class FloatInt extends Component {
   static template = "float_to_int_widget.FloatIntWidget";
+  static props = {
+        ...standardFieldProps,
+        rounding: { type: String, optional: true },
+    };
   setup() {
     super.setup();
     this.input = useRef("inputfloatint");
-    console.log(this.props.record.data[this.props.name],'1')
+    console.log(this.input)
     useInputField({
       getValue: () => this.props.record.data[this.props.name] || "",
       refName: "inputfloatint",
@@ -32,6 +38,7 @@ FloatInt.supportedOptions= [
         {
             label: _t("Round"),
             name: "round",
+//            type:"string"
             type: "selection",
             choices: [
                 { label: _t("Up"), value:"up"},
@@ -40,7 +47,31 @@ FloatInt.supportedOptions= [
             ],
         },
     ],
-
+FloatInt.extractProps=({options})=>({
+rounding:options.round})
 FloatInt.component = FloatInt;
 FloatInt.supportedTypes = ["float"];
+
+//export const FloatInt = {
+//    component: FloatInt,
+//    displayName: _t("FloatInt"),
+//    supportedTypes: ['float'],
+//    supportedOptions: [
+//        {
+//            label: _t("Round"),
+//            name: "round",
+////            type:"string"
+//            type: "selection",
+//            choices: [
+//                { label: _t("Up"), value:"up"},
+//                { label: _t("Down"), value:"down" },
+//                { label: _t("Default"), value:"default" },
+//            ],
+//        },
+//    ],
+//    extractProps: ({ options }) => ({
+//    rounding:options.round
+//    }),
+//};
+
 registry.category("fields").add("float_int_widget", FloatInt);
